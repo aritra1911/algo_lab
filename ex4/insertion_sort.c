4e65243b51fd9f8e0ffd15ceb2eb5254115439a2
Aritra Sarkar <aritra1911@yahoo.com>
2021-08-12 19:25:37 +0530

    Add question, and format table

--- ex4/insertion_sort.c
+++ ex4/insertion_sort.c
@@ -1,3 +1,50 @@
+/*
+ * Write a menu driven program as given below, to sort an array of n integers in
+ * ascending order by insertion sort algorithm and determine the time required
+ * (in terms of step/frequency count) to sort the elements. Repeat the
+ * experiment for different values of n and different nature of data (i.e.apply
+ * insertion sort algorithm on the data of array that are already sorted,
+ * reversly sorted and random data). Finally plot a graph of the time taken
+ * versus n for each type of data. The elements can be read from a file or can
+ * be generated using the random number generator.
+ *
+ * INSERTION SORT MENU
+ *
+ * 0. Quit
+ * 1. n Random numbers=>Array
+ * 2. Display the Array
+ * 3. Sort the Array in Ascending Order by using Insertion Sort
+ *    Algorithm
+ * 4. Sort the Array in Descending Order by using any sorting
+ *    algorithm
+ * 5. Time Complexity to sort ascending of random data
+ * 6. Time Complexity to sort ascending of data already sorted in
+ *    ascending order
+ * 7. Time Complexity to sort ascending of data already sorted in
+ *    descending order
+ * 8. Time Complexity to sort ascending of data for all Cases (Data
+ *    Ascending, Data in Descending & Random Data) in Tabular form
+ *    for values n=5000 to 50000, step=5000
+ * Enter your choice:
+ * If the choice is option 8, the it will display the tabular form as follows:
+ *
+ *              Analysis of Insertion Sort Algorithm
+ * Sl.   Value   Time Complexity   Time Complexity   Time Complexity
+ * No.   of n       (Data in          (Data in        (Random Data)
+ *                  Ascending)       Descending)
+ * -----------------------------------------------------------------
+ *   1    5000
+ *   2   10000
+ *   3   15000
+ *   4   20000
+ *   5   25000
+ *   6   30000
+ *   7   35000
+ *   8   40000
+ *   9   45000
+ *  10   50000
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -37,23 +84,26 @@ int main(void)
     int *arr = NULL;
     int n=0, flags=0;
     clock_t start, end;
+    double rand_time, asc_time, desc_time;
 
-    printf("INSERTION SORT MENU\n"
-           "0. Quit\n\n"
-           "1. n Random numbers => Array\n\n"
-           "2. Display the Array\n\n"
+    printf("--------------------------------------------------------------\n"
+           "                     INSERTION SORT MENU\n"
+           "0. Quit\n"
+           "1. n Random numbers => Array\n"
+           "2. Display the Array\n"
            "3. Sort the Array in Ascending Order by using Insertion Sort\n"
-           "   Algorithm\n\n"
+           "   Algorithm\n"
            "4. Sort the Array in Descending Order by using any sorting\n"
-           "   algorithm\n\n"
-           "5. Time Complexity to sort ascending of random data\n\n"
+           "   algorithm\n"
+           "5. Time Complexity to sort ascending of random data\n"
            "6. Time Complexity to sort ascending of data already sorted in\n"
-           "   ascending order\n\n"
+           "   ascending order\n"
            "7. Time Complexity to sort ascending of data already sorted in\n"
-           "   descending order\n\n"
+           "   descending order\n"
            "8. Time Complexity to sort ascending of data for all Cases\n"
            "   (Data Ascending, Data in Descending & Random Data) in\n"
-           "   Tabular form for values n=5000 to 50000, step=5000\n\n");
+           "   Tabular form for values n=5000 to 50000, step=5000\n"
+           "--------------------------------------------------------------\n");
 
     while ( 1 ) {
         printf("> ");
@@ -224,48 +274,54 @@ int main(void)
             case '8':
                 getchar();  /* eat trailing newline */
 
-                {
-                    double rand_time, asc_time, desc_time;
+                /* print header */
+                printf("             Analysis of Insertion Sort Algorithm\n"
+                       "Sl.   Value   Time Complexity   Time Complexity   Time "
+                       "Complexity\nNo.   of n       (Data in          (Data in"
+                       "        (Random Data)\n                 Ascending)     "
+                       "  Descending)\n----------------------------------------"
+                       "-------------------------\n");
 
-                    for (int n = 5000; n <= 50000; n += 5000) {
-                        /* Initialize RNG seed */
-                        srand((unsigned) time(NULL));
+                int slno = 1;
+                for (int n = 5000; n <= 50000; n += 5000) {
+                    /* Initialize RNG seed */
+                    srand((unsigned) time(NULL));
 
-                        int *arr = malloc(n * sizeof *arr);
+                    int *arr = malloc(n * sizeof *arr);
 
-                        /* Generate `n` random numbers */
-                        for (int i = 0; i < n; i++) {
-                            arr[i] = rand();
-                        }
-
-                        /* Time sorting in ascending */
-                        start = clock();
-                        insertion_sort(arr, n);
-                        end = clock();
-                        rand_time = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-                        /* Time sorting in ascending again */
-                        start = clock();
-                        insertion_sort(arr, n);
-                        end = clock();
-                        asc_time = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-                        /* Sort that in descending */
-                        qsort(arr, n, sizeof *arr, compare_desc);
-
-                        /* Sort the descending back in ascending */
-                        start = clock();
-                        insertion_sort(arr, n);
-                        end = clock();
-                        desc_time = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-                        printf("n=%d   %lf   %lf   %lf\n",
-                               n, asc_time, desc_time, rand_time);
-
-                        free(arr);
+                    /* Generate `n` random numbers */
+                    for (int i = 0; i < n; i++) {
+                        arr[i] = rand();
                     }
 
-                } break;
+                    /* Time sorting in ascending */
+                    start = clock();
+                    insertion_sort(arr, n);
+                    end = clock();
+                    rand_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+                    /* Time sorting in ascending again */
+                    start = clock();
+                    insertion_sort(arr, n);
+                    end = clock();
+                    asc_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+                    /* Sort that in descending */
+                    qsort(arr, n, sizeof *arr, compare_desc);
+
+                    /* Sort the descending back in ascending */
+                    start = clock();
+                    insertion_sort(arr, n);
+                    end = clock();
+                    desc_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+                    printf("%3d   %5d   %15lf   %15lf   %15lf\n",
+                           slno++, n, asc_time, desc_time, rand_time);
+
+                    free(arr);
+                }
+
+                break;
 
             case EOF: printf("0\n");
             default: goto _exit;
