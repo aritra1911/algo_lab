5d5010c46ce89db5bebd5ab5e70f6698ed5879c5
Aritra Sarkar <aritra1911@yahoo.com>
2021-08-23 18:23:59 +0530

    Implement a basic recursive binary search

---
+++ ex5/bin_search.c
@@ -0,0 +1,51 @@
+/*
+ * Write a program to search an element x in an array of n integers using Binary
+ * Search algorithm that uses divide and conquer technique. Find out the best
+ * case, worst case and average case time complexities for different values of n
+ * and plot a graph of the time taken versus n. The n integers can be generated
+ * randomly and x can be choosen randomly, or any element of the array or middle
+ * or last element of the array depending on type of time complexity analysis.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int bin_search(int item, int *arr, int len)
+{
+    /* This algorithm assumes that `arr` is sorted in ascending order */
+
+    if ( len ) {
+        int m = (len - 1) >> 1;
+
+        if (arr[m] == item)
+            return 0;
+
+        if (arr[m] < item)
+            return bin_search(item, arr + m + 1, len - m - 1);
+
+        return bin_search(item, arr, len - m - 1);
+    }
+
+    return -1;
+}
+
+int main(int argc, char** argv)
+{
+    int item = atoi(argv[1]);
+
+    int *arr = malloc((argc - 2) * sizeof *arr);
+
+    for (int i = 2; i < argc; i++) {
+        arr[i - 2] = atoi(argv[i]);
+    }
+
+    if ( bin_search(item, arr, argc - 2) != -1 ) {
+        printf("Found!\n");
+    } else {
+        printf("Not Found!\n");
+    }
+
+    free(arr);
+
+    return EXIT_SUCCESS;
+}
