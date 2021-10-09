42e114ea1f231d715fd8f222def34a1c425f1770
Aritra Sarkar <aritra1911@yahoo.com>
2021-10-09 15:19:03 +0530

    Print table and foind least length

--- ex8/intervals.c
+++ ex8/intervals.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 typedef struct {
     size_t index;
@@ -21,11 +22,23 @@ typedef struct {
 int compare_intervals(const void *, const void *);
 static Interval *get_interval_by_index(size_t, Interval *, size_t);
 
+int length(const Interval *interval)
+{
+    int len = interval->end - interval->start;
+    return len;
+}
+
+char *to_string(const Interval *interval, char *str)
+{
+    sprintf(str, "(%c, %i, %i)", interval->name,
+            interval->start, interval->end);
+
+    return str;
+}
+
 int compare_intervals(const void *i1, const void *i2)
 {
-    int len_i1 = ((Interval *) i1)->end - ((Interval *) i1)->start;
-    int len_i2 = ((Interval *) i2)->end - ((Interval *) i2)->start;
-    return len_i1 < len_i2 ? -1 : len_i1 > len_i2 ? 1 : 0;
+    return ((Interval *) i1)->end - ((Interval *) i2)->end;
 }
 
 static Interval *get_interval_by_index(size_t idx, Interval *intervals,
@@ -44,14 +57,15 @@ int main(void)
 {
     size_t n;
     Interval *intervals;
+    int least_length;
 
-    printf("Enter number of intervals: ");
+    printf("Enter maximum number of intervals: ");
     scanf("%lu", &n);
     getchar();  /* eat trailing '\n' */
 
     intervals = calloc(n, sizeof (Interval));
 
-    printf("\nEnter intervals:\n");
+    printf("\nEnter name of intervals with their start and end number:\n");
     for (size_t i = 0; i < n; i++) {
         intervals[i].index = i;
         scanf("(%c,%i,%i)", &intervals[i].name,
@@ -59,11 +73,34 @@ int main(void)
         getchar();  /* eat trailing '\n' */
     }
 
+    qsort(intervals, n, sizeof (Interval), compare_intervals);
+
+    printf("\nThe intervals with their lengths sorted in ascending order "
+           "with their end number.\nIntervals          Length of Intervals\n");
+    least_length = length(&intervals[0]);
+    for (size_t i = 0; i < n; i++) {
+        char interval_str[16];
+        int len = length(&intervals[i]);
+        assert( len > 0 );
+        printf("%-16s   %i\n", to_string(&intervals[i], interval_str),
+                               length(&intervals[i]));
+        if ( len < least_length )
+            least_length = len;
+    }
+
+    printf("\nThe least length intervals : { ");
+    for (size_t i = 0; i < n; i++) {
+        if ( length(&intervals[i]) == least_length )
+            printf("%c, ", intervals[i].name);
+    } printf("}\n");
+
+/*
     for (size_t i = 0; i < n; i++) {
         printf("\nindex : %lu\n name : %c\nstart : %i\n  end : %i\n",
                intervals[i].index, intervals[i].name,
                intervals[i].start, intervals[i].end);
     }
+*/
 
     free(intervals);
 
